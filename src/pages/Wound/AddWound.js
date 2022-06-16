0f33b71e3e0a77dfb39b17553e53923b0ac71a82
CamielVilla <camielvilla@gmail.com>
2022-06-16 21:16:30 +0200

    post wound added

--- src/pages/Wound/AddWound.js
+++ src/pages/Wound/AddWound.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import "./AddWound.css";
 import {useForm} from "react-hook-form";
 import Form from "../../components/Form/Form";
 import TextInput from "../../components/Form/TextInput";
 import TextAreaInput from "../../components/Form/TextAreaInput";
 import Button from "../../components/Button/Button";
 import Page from "../../components/Page/Page";
+import axios from "axios";
 
 function AddWound() {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [file, setFile] = useState("");
+    const [previewUrl, setPreviewUrl] = useState("");
+
+    function handleImageChange (e){
+        const uploadedFile = e.target.files[0];
+        console.log(uploadedFile)
+        setFile(uploadedFile);
+        setPreviewUrl(URL.createObjectURL(uploadedFile));
+    }
+
+    async function sendImage(e){
+        const formData = new formData();
+        formData.append("file", file)
+
+        try {
+            // verstuur ons formData object en geef in de header aan dat het om een form-data type gaat
+            // Let op: we wijzigen nu ALTIJD de afbeelding voor student 1001, als je een andere student wil kiezen of dit dynamisch wil maken, pas je de url aan!
+            const result = await axios.post('http://localhost:8080/patients/1002/wound', formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    },
+                })
+            console.log(result.data);
+        } catch (e) {
+            console.error(e)
+        }
+    }
 
 
     function addNewWound(){
@@ -15,6 +45,7 @@ function AddWound() {
     }
     return (
         <Page>
+
         <Form
         title="Voeg nieuwe wond toe"
         handleSubmit={handleSubmit(addNewWound)}
@@ -41,6 +72,15 @@ function AddWound() {
                 maximLength={50}
                 isRequired={true}
             />
+            {/*<TextInput*/}
+            {/*    htmlFor="wound-image"*/}
+            {/*    type="file"*/}
+            {/*    register={register}*/}
+            {/*    errors={errors}*/}
+            {/*    isRequired={true}*/}
+            {/*/>*/}
+
+
             <TextAreaInput
                 htmlFor="wound-plan"
                 placeholder="Behandelplan"
@@ -53,6 +93,22 @@ function AddWound() {
                 cols={40}
                 rows={30}
             />
+
+            {/*<label htmlFor="wound-image">*/}
+            {/*    <input type="file"*/}
+            {/*           id="wound-image"*/}
+            {/*           name="image-field"*/}
+            {/*           onChange={handleImageChange}*/}
+            {/*    />*/}
+            {/*</label>*/}
+            {previewUrl &&
+                <label>
+                    Preview:
+                    <img
+                        src={previewUrl}
+                        alt="voorbeeld van de afbeelding" className="image-preview" />
+                </label>
+            }
             <div className="button-container">
                 <Button buttonType="reset">Reset</Button>
                 <Button buttonType="submit">Voeg toe</Button>
